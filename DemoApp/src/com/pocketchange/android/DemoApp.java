814211f269d84c00a50f0b05c14190d0527ff127
Justin Garen <justin@pocketchange.com>
2012-05-08 19:47:57 -0700

    minor updates to demo app to match SDK version 4.0.0

--- DemoApp/src/com/pocketchange/android/DemoApp.java
+++ DemoApp/src/com/pocketchange/android/DemoApp.java
@@ -1,8 +1,6 @@
 package com.pocketchange.android;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,7 +14,7 @@ public class DemoApp extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        PocketChange.initializeInTestMode(getApplicationContext(), APP_ID);
+        PocketChange.initialize(this, APP_ID);
     }
 
     @Override
@@ -32,10 +30,10 @@ public class DemoApp extends Activity {
     }
 
     public void startGame(final View view) {
-      if (PocketChange.takeTurn()) {
-          // Note: Ordinarily, this block should actually start a game instead
-          // of showing a toast.
-          Toast.makeText(this, "Starting game", Toast.LENGTH_SHORT).show();
-      }
+        if (PocketChange.takeTurn()) {
+            // Note: Ordinarily, this block should actually start a game instead
+            // of showing a toast.
+            Toast.makeText(this, "Starting game", Toast.LENGTH_SHORT).show();
+        }
     }
 }
